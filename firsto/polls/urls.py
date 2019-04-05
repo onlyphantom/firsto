8ac18c42e7f624f41a1827146336d3ac0cbb4dfb
onlyphantom <samuel@outlook.sg>
2019-04-05 18:30:09 +0700

    complete part 3

--- firsto/polls/urls.py
+++ firsto/polls/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('abc', views.abcview, name='abcview')
+    # eg: /polls/
+    path('', views.index, name='polls-index'),
+    # eg: /polls/5/
+    # invoke: detail(<HttpRequest>, question_id)
+    # angle brackets "< >" captures and send as kw argument
+    path('<int:question_id>/', views.detail, name='polls-detail'),
+    # eg: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='polls-results'),
+    # eg: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='polls-vote'),
 ]
