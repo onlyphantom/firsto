ecd68d18b1c7a419e339e57e2427081a825ef09c
onlyphantom <samuel@outlook.sg>
2019-04-06 16:05:57 +0700

    add polls-vote view function

--- firsto/polls/views.py
+++ firsto/polls/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.urls import reverse
+from .models import Choice, Question
 
 
 def index(request):
@@ -21,7 +22,8 @@ def index(request):
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/detail.html',
+                  {'question': question, 'elems': ['a', 'b', 'd']})
 
 
 # def detail(request, question_id):
@@ -39,4 +41,20 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Return HttpResponseRedirect after POST preventing data from posted
+        # twice if user hits the Back button
+        return HttpResponseRedirect(reverse('polls:polls-results',
+                                    args=(question.id)
+        ))
