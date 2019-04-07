0a227bbe1cefb6e1d3f74e0ff8cd9574f1abf97d
onlyphantom <samuel@outlook.sg>
2019-04-07 19:00:17 +0700

    extend from Django.admin; part 7

--- firsto/polls/admin.py
+++ firsto/polls/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        (
+            'Date information',
+            {'fields': ['pub_date'],
+             'classes': ['collapse']}
+        ),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date', 'question_text']
+    search_fields = ['question_text']
+
+
+admin.site.site_header = 'Polls'
+admin.site.register(Question, QuestionAdmin)
